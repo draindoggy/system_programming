0c2d3565275f0384d210056342c4877a6f852e03
draindoggy <119752646+draindoggy@users.noreply.github.com>
2023-05-19 12:49:56 +0300

    Update лабораторная работа №5. пункт 1.cpp

--- лабораторная работа №5. пункт 1.cpp
+++ лабораторная работа №5. пункт 1.cpp
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
@@ -9,25 +9,25 @@ int main() {
     int n;
     printf("введите размер массива: ");
     scanf_s("%d", &n);
-    int* array = (int*)malloc(n * sizeof(int)); // Динамический массив
+    int* array = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         printf("a[%d] = ", i);
         scanf_s("%d", &array[i]);
     }
     int sum = sumOfOdd(array, n);
     printf("сумма нечетных элементов массива: %d", sum);
-    free(array); // Освободить выделенную память
+    free(array); 
     return 0;
 }
 
 int sumOfOdd(int* array, int n) {
     if (n == 0) {
-        return 0; // Базовый случай рекурсии
+        return 0; 
     }
     else {
         int sum = sumOfOdd(array, n - 1);
         if (n - 1 >= 0 && array[n - 1] % 2 != 0) {
-            return sum + array[n - 1]; // Рекурсивный случай
+            return sum + array[n - 1]; 
         }
         else {
             return sum;
