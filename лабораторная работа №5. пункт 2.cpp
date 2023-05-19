945bc790bc2da9336843fb351628279b32b58740
draindoggy <119752646+draindoggy@users.noreply.github.com>
2023-05-19 12:45:51 +0300

    Add files via upload

---
+++ лабораторная работа №5. пункт 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+void reverseNumber(int n) {
+    if (n == 0) {
+        return; // Базовый случай рекурсии
+    }
+    else {
+        printf("%d", n % 10);
+        reverseNumber(n / 10); // Рекурсивный случай
+    }
+}
+
+int main() {
+    int number;
+    setlocale(0, "rus");
+    printf("введите число: ");
+    scanf_s("%d", &number);
+    printf("число %d в обратном порядке: ", number);
+    reverseNumber(number);
+    printf("\n");
+    return 0;
+}
